c410297a552cef1e38674b6eee223315e31d3ef3
at15 <at15@outlook.com>
2018-04-26 11:47:59 -0700

    [vue] Import data from js file in component
    - NOTE: vue will add attributes like `__ob__`, getter and setters to object
      - might be better to use a shallow/deep copy

--- vuepress/1-awesome/.vuepress/components/foo-1.vue
+++ vuepress/1-awesome/.vuepress/components/foo-1.vue
@@ -1,16 +1,39 @@
 <template>
+<div>
   <p>
       I am {{ foo }}
   </p>
+  awesome databases
+  <table>
+      <thead>
+          <tr>
+              <th>Name</th>
+              <th>Language</th>
+              <th>Backend</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr v-for="db in databases">
+            <td>{{ db.name }}</td>
+            <td>{{ db.lang }}</td>
+            <td>{{ db.backend }}</td>
+          </tr>
+      </tbody>
+  </table>
+</div>
 </template>
 
 <script>
+import { databases } from "../../data/databases";
+
 export default {
   data() {
-      return {
-          foo: 'This is foo'
-      }
+    console.log(databases);
+    return {
+      foo: "This is foo",
+      databases: databases
+    };
   }
-}
+};
 </script>
 
